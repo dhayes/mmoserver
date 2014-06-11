bc2311d27cfba163174b02d4a30e382d02b61b3c
test <test@aaa.com>
2014-06-11 05:12:45 +0100

    boost working

--- mmoserver/src/mmoserver.cpp
+++ mmoserver/src/mmoserver.cpp
@@ -1,15 +1,25 @@
-//============================================================================
-// Name        : mmoserver.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+//
+// timer.cpp
+// ~~~~~~~~~
+//
+// Copyright (c) 2003-2013 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
 #include <iostream>
-using namespace std;
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main()
+{
+  boost::asio::io_service io;
+
+  boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+  t.wait();
+
+  std::cout << "Hello, world!\n";
+  //test
+  return 0;
 }
